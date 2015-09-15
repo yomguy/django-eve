78d5138591857ce3fe0cb9bae606a47170738882
Guillaume Pellerin <guillaume.pellerin@ircam.fr>
2015-09-15 12:29:59 +0200

    add models to admin

--- admin.py
+++ admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from django.db.models import get_models, get_app
 
-# Register your models here.
+for model in get_models(get_app('eve')):
+    admin.site.register(model)

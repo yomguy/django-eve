5f9951da4eb7afcd422aa79e4c73307616abb927
Guillaume Pellerin <guillaume.pellerin@ircam.fr>
2015-10-12 18:32:04 +0200

    add utils

---
+++ utils.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python
+# Copyright (C) 2015 Guillaume Pellerin <guillaume.pellerin@ircam.fr>
+# Licence: MIT
+
+
+import logging
+from auditlog.models import LogEntry
+
+
+class Logger:
+
+    def __init__(self, file):
+        self.logger = logging.getLogger('myapp')
+        self.hdlr = logging.FileHandler(file)
+        self.formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
+        self.hdlr.setFormatter(self.formatter)
+        self.logger.addHandler(self.hdlr)
+        self.logger.setLevel(logging.INFO)
+
+
+class AuditLogger(object):
+
+    def __init__(self, path, start_time):
+        self.log = open(path + '.audit', 'w')
+        self.start_time = start_time
+
+    def write(self):
+        logs = LogEntry.objects.filter(timestamp__gte=self.start_time)
+        for log in logs:
+            self.log.write('\n' + log.timestamp.__str__())
+            self.log.write(' : ' + log.object_repr + ' ' + str(log.object_id) + ' : ')
+            self.log.write(log.changes_str.encode('utf8'))
+        self.log.close()

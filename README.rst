ffd765e822be3f2cd8eff85e3876832174de9586
Guillaume Pellerin <guillaume.pellerin@ircam.fr>
2016-03-30 11:14:31 +0200

    update setup

---
+++ README.rst
@@ -0,0 +1,4 @@
+django-eve
+==========
+
+This is a django module implementing the models of E-venement edited by Libre-Informatique.
---
+++ setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import sys
+from setuptools import setup
+
+setup(
+    name='django-eve',
+    url='https://forge.ircam.fr/p/django-eve/',
+    description="Django module implementing the E-venement models (needed for Passerelle & co @IRCAM)",
+    long_description=open('README.rst').read(),
+    author="Guillaume Pellerin",
+    author_email="guillaume.pellerin@ircam.fr",
+    version='1.0',
+    install_requires=['django, ''],
+    platforms=['OS Independent'],
+    license='Gnu Public License V3',
+    packages=['django-eve'],
+    include_package_data=True,
+    zip_safe=False,
+    )
